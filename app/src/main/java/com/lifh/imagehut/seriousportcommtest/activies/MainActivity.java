497496fdc9c5359b048026642155a717e80479dd
Your Name <you@example.com>
2019-11-12 15:08:39 +0800

    a demo for Serious port comm

---
+++ app/src/main/java/com/lifh/imagehut/seriousportcommtest/activies/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lifh.imagehut.seriousportcommtest.activies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lifh.imagehut.seriousportcommtest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
